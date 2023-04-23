687861b6fe4f2bae61bf6bc5fde9217890a13bd9
subh839 <79743346+subh839@users.noreply.github.com>
2023-04-23 15:25:18 +0530

    done

--- routes/users.js
+++ routes/users.js
@@ -60,4 +60,15 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+
 export default router;
